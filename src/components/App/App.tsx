c42fbb8c499d525eeb274991b39c6b6cf772a65e
VadimLitau <vadimlitau@yandex.ru>
2024-06-06 20:33:49 +0500

    убрал лишнее

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,9 @@
-import useForm from "../../hooks/useForm";
 import style from "./App.module.scss";
 
 function App() {
-	const [values, setValues] = useForm();
-	console.log(values);
 	return (
 		<div className={style.mainWrap}>
 			<h1>Hello World</h1>
-			<input type="text" name="email" onChange={setValues} />
-			<input type="text" name="password" onChange={setValues} />
-			<input
-				type="checkbox"
-				style={{ border: "1px solid #ececec" }}
-				onChange={setValues}
-				name="rememberMe"
-			/>
 		</div>
 	);
 }
