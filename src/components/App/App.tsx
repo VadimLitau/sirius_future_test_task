70e91bc9a1e0cd2163d2a20e82cf643e00e00a33
VadimLitau <vadimlitau@yandex.ru>
2024-06-06 13:52:47 +0500

    добавил немного стилей

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,12 @@
+import Input from "../Input/Input";
+import style from "./App.module.scss";
+
 function App() {
 	return (
-		<>
+		<div className={style.mainWrap}>
 			<h1>Hello World</h1>
-		</>
+			<Input />
+		</div>
 	);
 }
 
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import style from "./input.module.scss";
+
+function Input() {
+	return (
+		<>
+			<input type="text" className={style.input} />
+		</>
+	);
+}
+
+export default Input;
